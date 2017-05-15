1a817b25a1b7a9167467e5b15ad8d139f3c10451
Ali Hawke <QuantumTea@users.noreply.github.com>
2017-05-15 08:20:10 -0500

    Update marsweather.js

--- marsweather.js
+++ marsweather.js
@@ -3,14 +3,16 @@ const fs = require('fs')
 // use readFileSync to force it to read it NOW
 var fromFile = fs.readFileSync('./mars_weather.json', 'utf8', err => {
     if (err) {
-        console.log("Oh noes! no such file");
+        console.log("Oh no! No such file");
         return console.log(err);
     } else {
         console.log("File exists");
     }
 })
 
-//console.log(fromFile)
 var jsonObject = JSON.parse(fromFile)
-console.log("\nOn Martian sol " + jsonObject.report.sol + " the max temp was " + jsonObject.report.max_temp + " Celsius")
-console.log("Martian sol " + jsonObject.report.sol + " equates to Earth date: " + jsonObject.report.terrestrial_date + "\n")
+console.log("\nOn Martian sol " + jsonObject.report.sol 
+	+ " the max temp was " + jsonObject.report.max_temp + " Celsius")
+
+console.log("Martian sol " + jsonObject.report.sol 
+	+ " equates to Earth date: " + jsonObject.report.terrestrial_date + "\n")
